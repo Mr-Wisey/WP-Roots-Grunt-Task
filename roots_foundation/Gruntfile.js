c3aed26c166a032ab1a011bdb947996d529c3e6e
Benjamin Unternährer <benjamin.unternaehrer@arcmedia.ch>
2015-01-26 15:57:46 +0100

    Add Roots Foundation

---
+++ roots_foundation/Gruntfile.js
@@ -0,0 +1,143 @@
+'use strict';
+module.exports = function(grunt) {
+  // Load all tasks
+  require('load-grunt-tasks')(grunt);
+  // Show elapsed time
+  require('time-grunt')(grunt);
+
+  var jsFileList = [
+    'assets/js/plugins/*.js',
+    'assets/js/_*.js',
+    'assets/js/plugins/foundation/*.js',
+  ];
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        '!assets/js/scripts.js',
+        '!assets/**/*.min.*'
+      ]
+    },
+    sass: {                              // Task
+        dev: {                            // Target
+          options: {                       // Target options
+            style: 'expanded'
+          },
+          files: {                         // Dictionary of files
+            'assets/css/normalize.css': 'assets/scss/normalize.scss',       // 'destination': 'source'
+            'assets/css/foundation.css': 'assets/scss/foundation.scss'       // 'destination': 'source'
+          }
+        },
+        build: {                            // Target
+          options: {                       // Target options
+            style: 'compressed'
+          },
+          files: {                         // Dictionary of files
+            'assets/css/normalize.min.css': 'assets/scss/normalize.scss',       // 'destination': 'source'
+            'assets/css/foundation.min.css': 'assets/scss/foundation.scss'       // 'destination': 'source'
+          }
+        }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [jsFileList],
+        dest: 'assets/js/scripts.js',
+      },
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [jsFileList]
+        }
+      }
+    },
+    modernizr: {
+      build: {
+        devFile: 'assets/vendor/modernizr/modernizr.js',
+        outputFile: 'assets/js/vendor/modernizr.min.js',
+        files: {
+          'src': [
+            ['assets/js/scripts.min.js'],
+            ['assets/css/foundation.min.css']
+          ]
+        },
+        extra: {
+          shiv: false
+        },
+        uglify: true,
+        parseFiles: true
+      }
+    },
+    version: {
+      default: {
+        options: {
+          format: true,
+          length: 32,
+          manifest: 'assets/manifest.json',
+          querystring: {
+            style: 'roots_css',
+            script: 'roots_js'
+          }
+        },
+        files: {
+          'lib/scripts.php': 'assets/{css,js}/{foundation,scripts}.min.{css,js}'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: [
+          'assets/scss/*.scss',
+          'assets/scss/**/*.scss',
+          'assets/scss/**/**/*.scss'
+        ],
+        tasks: ['sass:dev']
+      },
+      js: {
+        files: [
+          jsFileList,
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'concat']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: false
+        },
+        files: [
+          'assets/css/foundation.css',
+          'assets/js/scripts.js',
+          'templates/*.php',
+          '*.php'
+        ]
+      }
+    }
+  });
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'dev'
+  ]);
+  grunt.registerTask('dev', [
+    'jshint',
+    'sass:dev',
+    'concat'
+  ]);
+  grunt.registerTask('build', [
+    'jshint',
+    'sass:build',
+    'uglify',
+    'modernizr',
+    'version'
+  ]);
+};
---
+++ roots_foundation/templates/content-page.php
@@ -0,0 +1,7 @@
+<div class="row">
+  <div class="small-12 columns">
+<?php the_content(); ?>
+<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+  </div>
+</div>
+
